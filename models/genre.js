ba2da41cdb990d1ff3ab53560818c6a5fecc0044
Vasudev prajapat <parth21995@gmail.com>
2018-09-15 19:12:48 +0530

    Update genre.js

--- models/genre.js
+++ models/genre.js
@@ -33,7 +33,6 @@ module.exports.updateGenre = (id, genre, options, callback) => {
 	Genre.findOneAndUpdate(query, update, options, callback);
 }
 
-
 // Delete Genre
 module.exports.removeGenre = (id, callback) => {
 	var query = {_id: id};
